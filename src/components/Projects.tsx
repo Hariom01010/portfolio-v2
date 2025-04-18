fc82c1b6ca761d71742d0ebacfd51ead400b23c5
Hariom Vashista <rishivashista4@gmail.com>
2025-04-18 15:24:38 +0530

    fix broken links

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,10 +8,10 @@ function Projects() {
         <a href="https://www.knell.co.in" target="_blank">
           <motion.img src="./projects/knell.png" alt="knell-project-logo" className="rounded-full w-15 md:w-24 cursor-pointer shadow-lg shadow-green-600" animate={{x:[0,10,0], y:[0,20,0]}} transition={{repeat:Infinity, duration:3}}/>
         </a>
-        <a href="https://www.realdevsquad.com" target="_blank">
+        <a href="https://realdevsquad.com/" target="_blank">
           <motion.img src="./projects/rds-logo.png" alt="rds-logo" className="rounded-full w-15 md:w-24 cursor-pointer shadow-lg shadow-pink-600" animate={{x:[0,10,0], y:[0,20,0]}} transition={{repeat:Infinity, duration:3}} />
         </a>
-        <a href="https://team.shiksha.com" target="_blank">
+        <a href="https://team.shiksha/" target="_blank">
           <motion.img src="./projects/team-shiksha.png" alt="team-shiksha-logo" className="rounded-full w-15 md:w-24 cursor-pointer shadow-lg shadow-blue-600" animate={{x:[0,10,0], y:[0,20,0]}} transition={{repeat:Infinity, duration:3}} />
         </a>
       </div>
